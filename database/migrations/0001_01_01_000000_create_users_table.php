8fa9d01bf988d5e836f2888690211331d9bd646c
Timmsy1998 <jtimms1998@gmail.com>
2025-04-14 17:51:01 +0100

    Modified User Migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('country');
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->string('profile_picture')->nullable();
+            $table->string('password');            
             $table->rememberToken();
             $table->timestamps();
         });
